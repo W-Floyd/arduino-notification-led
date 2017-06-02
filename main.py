7403edb3a9693a4d78bbc6ba8877d001d66a0f68
William Floyd <william.png2000@gmail.com>
2017-06-02 20:51:29 +1000

    python: Chains together pulse commands to test queing.
    Signed-off-by: William Floyd <william.png2000@gmail.com>

--- main.py
+++ main.py
@@ -5,14 +5,7 @@ import time
 
 ser = serial.Serial('/dev/ttyUSB0', 9600)
 
+# We wait here because connecting to the Arduino takes a moment.
 time.sleep(2.5)
 
-ser.write('0,500,2,255')
-
-time.sleep(.1)
-
-ser.write('1,500,2,255')
-
-time.sleep(.1)
-
-ser.write('2,500,2,255')
+ser.write('0,1000,2,255-1,2000,2,255-2,100,2,255-1,100,5,64-')
